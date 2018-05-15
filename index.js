cc9fe3e4551a45b1ab88b2eec20c2cfb178177fe
Madeline McFadden <mcfaddem@iu.edu>
2018-05-15 11:34:28 -0400

    use color

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const handleSubmit = function(ev) {
   const form = ev.target
   const userName = form.userName.value
   const age = form.age.value
+  const favoriteColor = form.favoriteColor.value
   const users = document.querySelector('#users')
 
 const p = document.createElement('P')
-p.textContent = '${userName}, ${age}'
+p.textContent = '${userName}, ${age}, ${favoriteColor}'
+p.style.backgroundColor = favoriteColors
 
-  users.innerHTML += `<p>${userName}, ${age}</p>`
+  users.innerHTML += `<p>${userName}, ${age}, ${favoriteColor}</p>`
   form.reset()
   form.userName.focus()
 }
